31c7ffb81a165f717364889d2937dffba826c058
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-12-24 01:28:29 -0800

    if/else statement

--- app.js
+++ app.js
@@ -2,29 +2,44 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items = [];
+  let items = [];
+  let workItems = []
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.get("/", function(req, res){
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
     year:"numeric"
 
   };
-  var day = today.toLocaleDateString("en-us", options);
+  let day = today.toLocaleDateString("en-us", options);
 
- res.render("list", {kindOfDay: day, newListItems: items});
+ res.render("list", {listTitle: day, newListItems: items});
 });  
 
 app.post("/", function(req,res){
- let item = req.body.newItem;
- items.push(item);
-  res.redirect("/");
-});
+  let item = req.body.newItem;
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
+});
+app.get("/work", function(req,res){
+  res.render("list", {listTitle:"Work List", newListItems: workItems});
+});
+app.post("/work", function(req,res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 app.listen(3000, function(){
     console.log("server started on port 3000 😎");
 })
