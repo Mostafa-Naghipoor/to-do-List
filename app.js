c848c3e2c0a81a63ca5019eda4e84d596fecb892
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-12-24 20:23:23 -0800

    move date functioon to a new js file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const getDate = require("./date");
+const date = require (__dirname + "/date.js");
 const app = express();
   let items = [];
   let workItems = []
@@ -8,16 +9,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.get("/", function(req, res){
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year:"numeric"
-
-  };
-  let day = today.toLocaleDateString("en-us", options);
-
+  let day = date();
+ 
  res.render("list", {listTitle: day, newListItems: items});
 });  
 
@@ -40,6 +33,9 @@ app.post("/work", function(req,res){
   workItems.push(item);
   res.redirect("/work");
 })
+app.get("/about", function(req, res){
+  res.render("about");
+});
 app.listen(3000, function(){
     console.log("server started on port 3000 😎");
 })
